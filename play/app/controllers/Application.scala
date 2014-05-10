e6c1c6e0fa42cb27591bf9b03d7b24d8176522e4
Damien Engels <engelsdamien@gmail.com>
2014-05-11 01:29:13 +0200

    Adds routes and documentation for actions

--- play/app/controllers/Application.scala
+++ play/app/controllers/Application.scala
@@ -8,9 +8,98 @@ import play.api.Play.current
 import models.QuizzModel._
 import models.User
 
-object Application extends Controller {    
-        
-	
+object Application extends Controller {
+
+    /**
+     * @brief Gets a list of the groups a user belongs to
+     * 
+     * @input { token : text }
+     * 
+     * @output { groups : [ gid : int , name : text ]* }
+     * 
+     * @error { error : text }
+     */
+    def getListOfGroups = ???
+    
+     /**
+     * @brief Gets list of questions for a group
+     * 
+     * @input { token : text , gid : int }
+     * 
+     * @output { questions : [ qid : int , text : text , owner : int , upvote : int , downvote : int , tags : [ tag : text ]+ ]* }
+     * 
+     * @error { error : text }
+     */
+    def getListOfQuestions = ???
+    
+    /**
+     * @brief Gets list of answers for a question
+     * 
+     * @input { token : text , qid : int }
+     * 
+     * @output { questions : [ aid : int , text : text , owner : int , upvote : int , downvote : int ]* }
+     * 
+     * @error { error : text }
+     */
+    def getListOfAnswers = ???
+
+    /**
+     * @brief Insert new group
+     * 
+     * @input { token : text , name : text }
+     * 
+     * @output { gid : int }
+     * 
+     * @error { error : text }
+     */
+    def insertNewGroup = ???
+    
+    /**
+     * @brief Insert new question for a group
+     * 
+     * @input { token : text , gid : int , text : text , tags : [ tag : text ]+ }
+     * 
+     * @output { qid : int }
+     * 
+     * @error { error : text }
+     */
+    def insertNewQuestion = ???
+    
+    /**
+     * @brief Insert new answer for a question
+     * 
+     * @input { token : text , qid : int , text : text }
+     * 
+     * @output { aid : int }
+     * 
+     * @error { error : text }
+     */
+    def insertNewAnswer = ???
+    
+    
+    
+    /**
+     * @brief Generate a quizz based on a certain tag
+     * 
+     * @input { token : text , gid : int , tag : text }
+     * 
+     * @output { questions : [ aid : int , text : text , owner : int , upvote : int , downvote : int ]* }
+     * 
+     * @error { error : text }
+     */
+    def generateQuizz = ???
+    
+    /**
+     * @brief Proposes a list of tags for an incomplete tag
+     * 
+     * @input { token : text , tag : text , maxnb : int }
+     * 
+     * @output { tags : [ tag : text ]* }
+     * 
+     * @error { error : text }
+     */
+    def autoCompleteTag = ???
+
     def index = Action {
         Ok(views.html.index("Your new application is ready."))
     }
