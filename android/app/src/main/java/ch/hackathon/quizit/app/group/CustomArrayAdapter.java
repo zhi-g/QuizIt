73aa4a7d0e5442b0321c8c540e26d52a8465b3c3
MathieuMonney <m.monney@epfl.ch>
2014-05-10 23:16:09 +0200

    Implements the main activity and the group activity.

---
+++ android/app/src/main/java/ch/hackathon/quizit/app/group/CustomArrayAdapter.java
@@ -0,0 +1,54 @@
+package ch.hackathon.quizit.app.group;
+
+import android.content.Context;
+import android.database.DataSetObserver;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ch.hackathon.quizit.app.R;
+
+/**
+ * Created by mathieu on 10/05/14.
+ */
+public class CustomArrayAdapter extends ArrayAdapter<String> {
+    List<String> mGroupsList;
+    List<CustomListAdapterObserver> mObservers = new ArrayList<CustomListAdapterObserver>();
+    private Context mContext;
+
+    public interface CustomListAdapterObserver {
+        public void update();
+    }
+
+    public CustomArrayAdapter(Context context, List<String> groups, CustomListAdapterObserver observer) {
+        super(context, R.layout.custom_list_adapter_row_ayout, groups);
+        mContext = context;
+        mGroupsList = groups;
+        mObservers.add(observer);
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) mContext
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View rowView = inflater.inflate(R.layout.custom_list_adapter_row_ayout, parent, false);
+
+        return rowView;
+    }
+
+    public void notifyObserver() {
+        for(CustomListAdapterObserver observer: mObservers) {
+            observer.update();
+        }
+    }
+
+    public void joinGroup(View view) {
+
+    }
+}
