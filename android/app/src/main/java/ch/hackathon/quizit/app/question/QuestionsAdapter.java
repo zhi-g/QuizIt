0e5bc2f75cdb08d019a5cf4ea1926a4e6db0243c
ZhivkaGucevska <zhivka.gucevska@epfl.ch>
2014-05-11 00:09:19 +0200

    It doesn't work

--- android/app/src/main/java/ch/hackathon/quizit/app/question/QuestionsAdapter.java
+++ android/app/src/main/java/ch/hackathon/quizit/app/question/QuestionsAdapter.java
@@ -1,8 +1,12 @@
 package ch.hackathon.quizit.app.question;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.TextView;
 
 import ch.hackathon.quizit.app.R;
 
@@ -11,12 +15,14 @@ import ch.hackathon.quizit.app.R;
  */
 public class QuestionsAdapter extends ArrayAdapter<String> {
     Question[] questions;
+    Context  context;
 
 
 
     public QuestionsAdapter(Context context, Question[] questions) {
-        super(context, R.layout.row_layout_questions, extractQuestionText(questions));
+        super(context, R.layout.row_layout_questions);
         this.questions = questions;
+        this.context = context;
 
     }
 
@@ -34,6 +40,26 @@ public class QuestionsAdapter extends ArrayAdapter<String> {
       return questionsText;
     }
 
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View rowView = inflater.inflate(
+                R.layout.row_layout_questions, parent, false);
+        Button upvote = (Button) rowView
+                .findViewById(R.id.button_upvote);
+        Button downvote = (Button) rowView
+                .findViewById(R.id.button_downvote);
+        TextView questionText = (TextView) rowView
+                .findViewById(R.id.question_text);
+
+        upvote.setText("A");
+
+       questionText.setText(questions[position].getQuestionText());
+
+      return rowView;
+    }
 
 
  private class UpvoteButtonListener implements  View.OnClickListener {
